1051570e8bc944e3d234ecde4ae7d136d68ffd7c
luminorena <bakhuta.oa@gmail.com>
2023-04-20 23:45:25 +0300

    #3 Extra logging added

--- src/test/java/PetShop/Online/config/ProjectConfiguration.java
+++ src/test/java/PetShop/Online/config/ProjectConfiguration.java
@@ -3,6 +3,7 @@ package PetShop.Online.config;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+
 public class ProjectConfiguration {
 
 
@@ -14,20 +15,24 @@ public class ProjectConfiguration {
 
     public void webConfig() {
         org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(ProjectConfiguration.class);
+
+
         Configuration.baseUrl = webConfig.baseUrl();
         Configuration.browser = webConfig.browser();
         Configuration.browserVersion = webConfig.browserVersion();
 
-        logger.warn(Configuration.baseUrl = webConfig.baseUrl());
-        logger.warn(Configuration.browser = webConfig.browser());
-        logger.warn(Configuration.browserVersion = webConfig.browserVersion());
+        logger.warn("baseUrl ", Configuration.baseUrl = webConfig.baseUrl());
+        logger.warn("browser ",Configuration.browser = webConfig.browser());
+        logger.warn("browserVersion ",Configuration.browserVersion = webConfig.browserVersion());
 
     if (webConfig.isRemote()) {
         Configuration.remote = webConfig.remoteWebDriverUrl();
+        logger.warn("remoteUrl " ,Configuration.remote = webConfig.remoteWebDriverUrl());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+        logger.warn("capabilities ", capabilities);
         }
     }
 
