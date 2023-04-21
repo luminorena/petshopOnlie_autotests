a011713e3244b275d4334065609f8d46e96e032a
luminorena <bakhuta.oa@gmail.com>
2023-04-21 22:57:42 +0300

    #12 Added attachments

--- src/test/java/PetShop/Online/tests/TestBase.java
+++ src/test/java/PetShop/Online/tests/TestBase.java
@@ -3,9 +3,12 @@ package PetShop.Online.tests;
 import PetShop.Online.config.ProjectConfiguration;
 import PetShop.Online.config.WebConfig;
 import PetShop.Online.config.ConfigReader;
+import PetShop.Online.helpers.Attach;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -16,6 +19,19 @@ public class TestBase {
         SelenideLogger.addListener("allure", new AllureSelenide());
         ProjectConfiguration projectConfiguration = new ProjectConfiguration(webConfig);
         projectConfiguration.webConfig();
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
